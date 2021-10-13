a7df4fe52f271664f560c8052be2bd9a999ace6a
Mr-Destructive <gormeet711@gmail.com>
2021-10-13 12:00:17 +0530

    Send attachment file with emails

--- api/models.py
+++ api/models.py
@@ -11,27 +11,10 @@ class ScheduledMail(models.Model):
     sender = models.EmailField(max_length = 255) 
     subject = models.CharField(max_length = 78)
     body = models.CharField(max_length = 40000)
-    send_on = models.DateTimeField(default = datetime.now(),blank=True)
+    send_on = models.DateTimeField(default = datetime.now,blank=True)
     recipients_list = models.CharField(max_length = 1023)
 
     attachment_file = models.FileField(blank=True, null=True, upload_to='mail/uploads/')
-
-    @classmethod
-    def get_today_mail(cls):
-        today = date.today()
-        return cls.objects.filter(send_on__year = today.year, send_on__month = today.month, send_on__day = today.day)
-
-    def send_scheduled_mail(self):
-        recipient_list = list(self.recipients_list.values_list('mail_address', flat = True))
-        mail_msg = EmailMessage(
-                subject = self.subject,
-                body = self.body,
-                from_email = self.sender,
-                to = recipients_list,
-        )
-        mail_msg.content_subtype = 'html'
-
-        mail_msg.send()
 
 
     def get_absolute_url(self):  
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
 from rest_framework.response import Response
@@ -19,11 +19,28 @@ import json
 
 from django.utils import timezone
 from mail.tasks import sendmails
+
+from time import sleep
+
+
 # Create your views here.
 def handle_uploaded_file(f):  
-    with open('mail/upload/'+f.name, 'wb+') as destination:  
+    destination = 'mail/uploads/'
+    with open(f.name, 'wb+') as destination:  
         for chunk in f.chunks():  
             destination.write(chunk)  
+            
+
+def sendmails(sender_email, reciever_email, password, message, send_time):
+    port = 465
+    difference = send_time.timestamp() - datetime.now(timezone.utc).timestamp()
+    #sleep(difference)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        (server.sendmail(sender_email, reciever_email, message))
+
+
 
 class AddMailView(LoginRequiredMixin, CreateView):
     model = ScheduledMail
@@ -31,12 +48,13 @@ class AddMailView(LoginRequiredMixin, CreateView):
     template_name = 'mail/add.html'
     
     def form_valid(self, form):
+        super(AddMailView, self).form_valid(form)
         form.instance.sender = self.request.user.email
-        sendtime = form.instance.send_on
+        send_time = form.instance.send_on
         
         port = 465
         sender_email = self.request.user.email
-        reciever_email = form.instance.tags_list()
+        reciever_email = form.instance.recipients_list
         password = self.request.user.gapps_key
         body = form.instance.body
         subject = form.instance.subject
@@ -48,9 +66,13 @@ class AddMailView(LoginRequiredMixin, CreateView):
         
         message.attach(MIMEText(body, "plain"))
         
+
         file = form.instance.attachment_file
+        handle_uploaded_file(file)            
+
+        
         if file:
-            with open(file, "rb") as attachment:
+            with open(file.name,"rb") as attachment:
                 part = MIMEBase("application", "octet-stream")
                 part.set_payload(attachment.read())
 
@@ -58,27 +80,27 @@ class AddMailView(LoginRequiredMixin, CreateView):
 
             part.add_header(
                 "Content-Disposition",
-                f"attachment; filename= {filename}",
+                f"attachment; filename= {file}",
             )
 
+            
             message.attach(part)
-            message.attach(file.name, file.read(), file.content_type)
-
+            
         message = message.as_string()
 
-        
-        context = ssl.create_default_context()
-        
+        '''
+        loop = asyncio.new_event_loop()
+        async def create_tasks_func():
+            tasks = list()
+            tasks.append(asyncio.create_task(a_sendmails(sender_email, reciever_email, password, message, send_time)))
+            await asyncio.wait(tasks)
 
-        # instead of sending mails from here, we will call this later in the mails/tasks.py file to schedule
-        # sendmails(self.request, reciever_email, message, sendtime)
+        loop.run_until_complete(create_tasks_func())
+        loop.close()
+        '''
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, reciever_email, message)
+        sendmails(sender_email, reciever_email, password, message, send_time)
 
         return super(AddMailView, self).form_valid(form)
-
-
-
+        
 
